5677c18e880a5bff537b14d8781afea136e27880
Bruno Omella Mainieri <bruno.omella@hotmail.com>
2015-02-26 14:22:38 -0300

    Initial Commit

---
+++ LocationAndMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationAndMap
+//
+//  Created by Bruno Omella Mainieri on 2/26/15.
+//  Copyright (c) 2015 Bruno Omella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
